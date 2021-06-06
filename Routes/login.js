4dd8e7d3b8ba78fc77d2c38dc8764e02beeaf8d3
Shreyanshmmgn <smmgn7934@gmail.com>
2021-06-07 00:59:59 +0530

    cookie location chnged

--- Routes/login.js
+++ Routes/login.js
@@ -28,27 +28,22 @@ exports.login = (req, res, next) => {
               user.userData.name
             );
             const tokenObj = utils.issueJWT(user);
-            res.status(200).cookie("token", tokenObj.token, {
-              path: "/",
-              httpOnly: true,
-              secure: true,
-              expires: new Date(new Date().getTime() + 24 * 60 * 60 * 1000),
-            });
             res
               .status(200)
-              .cookie(
-                "uid",
-                user._id,
-                {
-                  path: "/",
-                  httpOnly: true,
-                  secure: true,
-                  expires: new Date(new Date().getTime() + 24 * 60 * 60 * 1000),
-                },
-                {
-                  domain: "http://localhost:3000",
-                }
-              )
+              .cookie("token", tokenObj.token, "http://localhost:3000", {
+                path: "/",
+                httpOnly: true,
+                secure: true,
+                expires: new Date(new Date().getTime() + 24 * 60 * 60 * 1000),
+              });
+            res
+              .status(200)
+              .cookie("uid", user._id, "http://localhost:3000", {
+                path: "/",
+                httpOnly: true,
+                secure: true,
+                expires: new Date(new Date().getTime() + 24 * 60 * 60 * 1000),
+              })
               .json({ userRegistered });
           } else {
             console.log("Wong password - 2");
